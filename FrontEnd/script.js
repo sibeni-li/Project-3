7a15af4e48c489e2ed8c7da35777dfcb7f2ed461
Lisa S <lisa.sibeni@gmail.com>
2024-04-29 15:24:16 +0200

    Ajout de commentaires + Boucle mise dans fonction

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,27 +1,35 @@
+//Récupération des données de l'API
 const response = await fetch("http://localhost:5678/api/works")
 console.log(response)
 const works = await response.json()
 console.log(works)
 
-for (let i = 0; i < works.length; i++){
-    const figure = works[i]
-    console.log(figure)
+function generWorks(){
 
-    const divGallery = document.querySelector(".gallery")
-
-    const worksElement = document.createElement("figure")
-
-    const imageElement = document.createElement("img")
-    imageElement.src = figure.imageUrl
-
-    const titleElement = document.createElement("figcaption")
-    titleElement.innerText = figure.title
-
-    divGallery.appendChild(worksElement)
-    worksElement.appendChild(imageElement)
-    worksElement.appendChild(titleElement)
-
-    console.log(worksElement)
-    console.log(imageElement)
-    console.log(titleElement)
+    for (let i = 0; i < works.length; i++){
+        const figure = works[i]
+        console.log(figure)
+        //Récupération de l'élément du DOM qui va accueillir les travaux de l'architecte
+        const divGallery = document.querySelector(".gallery")
+        //Création d'une balise dédiée à un des travaux
+        const worksElement = document.createElement("figure")
+        
+        const imageElement = document.createElement("img")
+        imageElement.src = figure.imageUrl
+        
+        const titleElement = document.createElement("figcaption")
+        titleElement.innerText = figure.title
+        //On rattache la balise figure à la div .gallery
+        divGallery.appendChild(worksElement)
+        //On rattache l'image et le titre a la balise figure
+        worksElement.appendChild(imageElement)
+        worksElement.appendChild(titleElement)
+        
+        console.log(worksElement)
+        console.log(imageElement)
+        console.log(titleElement)
+    }
 }
+
+generWorks()
+
