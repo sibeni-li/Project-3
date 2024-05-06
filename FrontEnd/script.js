8567330cba726045b659ab35f38f9e6faad28c7b
Lisa S <lisa.sibeni@gmail.com>
2024-05-06 14:45:25 +0200

    Modification fonction 'generWorks' avec forEach + Génération de filtres modifiée en 4 fonctions (une qui crée les boutons et qui gère le 'click', une qui gère les classes et indique quel filtre est sélectionné, une qui crée les filtres en appelant la première fonction et une qui appelle la création de filtres)

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -10,77 +10,85 @@ const categories = await respbis.json()
 console.log(categories) 
 
 //Fonction qui va générer les travaux
-function generWorks(works){
-
-    for (let i = 0; i < works.length; i++){
-        const figure = works[i]
+function generWorks(works) {
+    works.forEach((figure) => {
         console.log(figure)
-        //Récupération de l'élément du DOM qui va accueillir les travaux de l'architecte
+        // Récupération de l'élément du DOM qui va accueillir les travaux de l'architecte
         const divGallery = document.querySelector(".gallery")
-        //Création d'une balise dédiée à un des travaux
+        // Création d'une balise dédiée à un des travaux
         const worksElement = document.createElement("figure")
-        
+
         const imageElement = document.createElement("img")
         imageElement.src = figure.imageUrl
-        
+
         const titleElement = document.createElement("figcaption")
         titleElement.innerText = figure.title
-        //On rattache la balise figure à la div .gallery
+        // On rattache la balise figure à la div .gallery
         divGallery.appendChild(worksElement)
-        //On rattache l'image et le titre a la balise figure
+        // On rattache l'image et le titre à la balise figure
         worksElement.appendChild(imageElement)
         worksElement.appendChild(titleElement)
-        
+
         console.log(worksElement)
         console.log(imageElement)
         console.log(titleElement)
-        
+
         const categoryId = figure.category
         console.log(categoryId)
-        
-    }
+    })
 }
 
 generWorks(works)
 
-//Récupération de l'élément du DOM qui va accueillir les filtres
-const filters = document.querySelector(".filter")
-//Création de balises dédiées aux filtres
-const filterI = document.createElement("input")
-filterI.setAttribute("type", "button")
-filterI.setAttribute("value", "Tous")
-//On rattache la balise input à la div .filter
-filters.appendChild(filterI)
-console.log(filterI)
-
-filterI.addEventListener("click", () => {
-    document.querySelector(".gallery").innerHTML = ""
-    generWorks(works)
-})
-
-for ( let i = 0; i<categories.length; i++){
-    const input = categories[i]
-    console.log(input)
-    
+function createFilterButton(value, isAll) {
     const filterI = document.createElement("input")
     filterI.setAttribute("type", "button")
-    filterI.setAttribute("value",input.name)
-    filters.appendChild(filterI)
-    console.log(filterI)
-    
-    const id = input.id
-    console.log(id)
-    
-    filterI.addEventListener("click", () => {
-        console.log("click")
-        const filterCategory = works.filter(function(work){
-            return work.categoryId === id
-        })
-        console.log(filterCategory)
-        document.querySelector(".gallery").innerHTML = ""
-        generWorks(filterCategory)
+    filterI.setAttribute("value", value)
+    filterI.classList.add("noClick")
 
-    })
-    
+    if (isAll) {
+        filterI.addEventListener("click", () => {
+            document.querySelector(".gallery").innerHTML = ""
+            generWorks(works)
+            filterClass(filterI)
+        })
+    } else {
+        const id = categories.find((cat) => cat.name === value).id
+        filterI.addEventListener("click", () => {
+            const filterCategory = works.filter((work) => work.categoryId === id)
+            document.querySelector(".gallery").innerHTML = ""
+            generWorks(filterCategory)
+            filterClass(filterI)
+        })
+    }
+
+    return filterI
 }
 
+function filterClass(filterElement) {
+    const allFilters = document.querySelectorAll(".filter input")
+    allFilters.forEach((filter) => {
+        filter.classList.remove("click")
+        filter.classList.add("noClick")
+    })
+
+    filterElement.classList.remove("noClick")
+    filterElement.classList.add("click")
+}
+
+function createFilters(categories) {
+    const filters = document.querySelector(".filter")
+    const filterAll = createFilterButton("Tous", true)
+    filters.appendChild(filterAll)
+
+    categories.forEach((input) => {
+        const filter = createFilterButton(input.name, false)
+        filters.appendChild(filter)
+    })
+}
+
+function filters(categories) {
+    createFilters(categories)
+}
+
+filters(categories)
